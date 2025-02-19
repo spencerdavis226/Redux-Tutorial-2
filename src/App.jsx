bebd014de82328d817121916df38b87607342a6f
Spencer Davis <spencerdavis226@gmail.com>
2025-02-19 13:27:11 -0500

    Added actions.jsx, actiontypes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
 import store from './Store';
-import { bugAdded } from './actions';
-
-const unsubscribe = store.subscribe(() => {
-  console.log('Store changed!', store.getState());
-});
+import { bugAdded, bugResolved } from './actions';
 
 store.dispatch(bugAdded('Bug 1'));
-
-unsubscribe();
-
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+store.dispatch(bugResolved(1));
 console.log(store.getState());
 
 export default App;
--- src/actions.jsx
+++ src/actions.jsx
@@ -3,6 +3,13 @@ import * as actions from './actiontypes';
 export const bugAdded = (description) => ({
   type: actions.BUG_ADDED,
   payload: {
-    description: 'Bug1',
+    description: description, // or shorthand code to just: description
+  },
+});
+
+export const bugResolved = (id) => ({
+  type: actions.BUG_RESOLVED,
+  payload: {
+    id: id,
   },
 });
